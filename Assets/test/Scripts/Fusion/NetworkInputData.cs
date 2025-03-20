413f9b0afbdb32f91c8a5856d1f9568cdd659ad7
Anastasiia <holubieva.a.a@gmail.com>
2025-03-20 15:57:25 +0200

    Added UI

--- Assets/test/Scripts/Fusion/NetworkInputData.cs
+++ Assets/test/Scripts/Fusion/NetworkInputData.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public struct NetworkInputData : INetworkInput
 {
     public const byte MOUSEBUTTON1 = 2;
+    public const byte SHOOT = 1;
 
     public NetworkButtons buttons;
     public Vector3 direction;
+    public Vector3 shootDirection;
 }
